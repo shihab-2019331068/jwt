a316ddc962571b14eac047947d0b842ecd554e60
shihab-2019331068 <siababdullah3946@gmail.com>
2025-05-27 22:20:36 +0600

    second commit

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 
+
+
 const posts = [
     {
         username: 'Shihab',
@@ -26,8 +28,8 @@ const posts = [
     }
 ]
 
-app.get('/post', (req, res) => {
-    res.json(posts);
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
 })
 
 app.post('/login', (req, res) => {
@@ -41,6 +43,17 @@ app.post('/login', (req, res) => {
 
 })
 
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1] // Bearer TOKEN
+    
+    if (token == null) return res.sendStatus(401) // Unauthorized
 
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403) // Forbidden
+        req.user = user
+        next()
+    })
+}
 
 app.listen(3000)
